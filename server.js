d15b6ba8339c286061c67f678ff3b83859e35c89
Bryant Novas <bryant.novas@gmail.com>
2019-12-14 00:37:18 -0500

    Type: fix
    changes port env config for heroku.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var port = (process.env.port || 3000);
+var port = (process.env.PORT || 8080);
 
 app.use(express.static('public'))
 app.get('/', function(req, res){
